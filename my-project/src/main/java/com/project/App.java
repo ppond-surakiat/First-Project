b37a7fa44c5970517ab23195b37dc2f22d8cfb8a
ppond-surakiat <pangpond.surakiat@gmail.com>
2026-02-13 23:56:30 +0700

    Build 0.1 (Make branch - Maven)

---
+++ my-project/src/main/java/com/project/App.java
@@ -0,0 +1,10 @@
+package com.project;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, This is my first Java project on the first git repository!" );
+        System.out.println( "Thanks for watching." );
+    }
+}
