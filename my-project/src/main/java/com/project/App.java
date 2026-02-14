37762d8bb1aa4bc5d2922ab3d039e6fcca5ac82f
ppond-surakiat <pangpond.surakiat@gmail.com>
2026-02-14 11:42:09 +0700

    Build 0.3 (Update com.project.App)

--- my-project/src/main/java/com/project/App.java
+++ my-project/src/main/java/com/project/App.java
@@ -1,10 +1,39 @@
 package com.project;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println( "Hello, This is my first Java project on the first git repository!" );
-        System.out.println( "Thanks for watching." );
+        System.out.println( "Thanks for watching.\n" );
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        if(age >= 20) {
+            System.out.print("Are you sure you are an adult? (yes/no): ");
+            String isSure = sc.next();
+            if(isSure.equalsIgnoreCase("yes")) {
+                System.out.print("Enter your country: ");
+                String country = sc.next();
+                System.out.println("Hello " + name + ", you are an adult from " + country + ".");
+            } else {
+                System.out.println("You are not an adult.");
+            }
+         } else if(age >= 13 && age < 20) {
+            System.out.println("Hello " + name + ", you are a teenager.");
+        } else {
+            System.out.println("Hello " + name + ", you are a minor.");
+        }
+
+        sc.close();
+
     }
 }
